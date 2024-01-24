444295a46240026279f9e7649dbd3186edd79c8f
HannaFarha <johnnyfarha1@gmail.com>
2024-01-24 16:18:43 +0100

    user.models done

---
+++ Furrow/models/User.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    name: { type: String, required: true },
+  },
+  
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
+
