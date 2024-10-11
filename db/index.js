fc08514faa5fefb14285d0bc65c21ec9f4617cc8
HannaFarha <johnnyfarha1@gmail.com>
2024-10-11 13:24:45 +0200

    done

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Todo'
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb+srv://johnnyfarha992:J5BmpnF7CE3Ibj3o@mongo.n8j2u.mongodb.net/mongo?retryWrites=true&w=majority'
 
 const withDB = async serverListener => {
   try {
